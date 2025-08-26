ee0ac4fe736b1145833994590ab958fdc0486324
khantauqeer272@gmail.com <khantauqeer272@gmail.com>
2025-08-26 09:55:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: '1000logos.net',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
